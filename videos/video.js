7619fc9d93bdcafa2854a7b4ba651f6b5367e144
Irina <upsk237p168k5@iscte-iul.pt>
2023-02-08 12:47:26 +0000

    Config

--- videos/video.js
+++ videos/video.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const query = require("../query");
 
 router.get('/:id', async function (req, res) {
-    let video = await query("select * FROM video WHERE id = ?", [req.params.id]);
+    let video = await query("select id, titulo as title, categoria as categories, url_youtube as video, canal as channel_id, miniatura as logo, data_publicacao as date, descricao as description FROM video WHERE id = ?", [req.params.id]);
     res.json(video);
 });
 
