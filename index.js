3c87d4bca10cede87de796e49cd9fea9f2d4c1f7
Irina <upsk237p168k5@iscte-iul.pt>
2023-02-08 10:26:03 +0000

    Adding routes

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/autores", require("./autores.js"));
+app.use("/livros", require("./livros.js"));
+app.use("/livros", require("./livros.js"));
+
+app.listen(3000);
